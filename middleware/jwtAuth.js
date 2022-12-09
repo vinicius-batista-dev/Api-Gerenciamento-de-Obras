aa37e05bc880e9e2f7f381f4fd0dbdb57808e029
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-12-09 16:00:00 -0300

    configurando o token do usuario

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -4,36 +4,45 @@ const database = require("../models");
 
 const User = database.user;
 
-//Deve verificar se o usuario ja existee nao deixar acessar os dados de outro usuario
-const verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
-  if (!token) {
-    return res.status(403).send({
-      message: "Nenhum token fornecido!",
-    });
-  }
-
-  if (token.startsWith("Bearer ")) {
-    // Remove Bearer from string
-    token = token.slice(7, token.length);
-  }
-
-  if (token === "null") {
-    return res.status(401).send({
-      message: "Não autorizado!",
-    });
-  }
-
-  jwt.verify(token, configuration.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({
-        message: "Não autorizado!",
+    if (!token) {
+      throw res.status(403).send({
+        message: "token nao econtrado!",
       });
     }
-    req.userId = decoded.id;
-    next();
-  });
+
+    const tokenExists = jwt.verify(token, configuration.secret);
+    if (!tokenExists) {
+      throw res.status(403).send({
+        message: "token invalido!",
+      });
+    }
+
+    await User.findOne({
+      where: {
+        id: tokenExists.id,
+      },
+    }).then((user) => {
+      if (!user) {
+        return res.status(400).send({
+          message: "usuario nao econtrado",
+        });
+      } else if (user.token !== token) {
+        return res.status(400).send({
+          message: "token expirado",
+        });
+      }
+      req.userId = tokenExists.id;
+      next();
+    });
+  } catch (error) {
+    throw res.status(403).send({
+      message: "token nao econtrado!",
+    });
+  }
 };
 
 const jwtAuth = {
