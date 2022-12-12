bfd016d557cdde048da09064c9a29a57eb4d28cb
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-12-11 23:37:15 -0300

    atualizando as configuracoes do banco

--- database/config-db.js
+++ database/config-db.js
@@ -1,8 +1,8 @@
 module.exports = {
-  host: "127.0.0.1",
-  user: "vinicius",
-  password: "admin123",
-  database: "api-gerenciamento-obras",
+  host: "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "fcf6apmfhdutisij",
+  password: "sz0ygtwq60zzl4cz",
+  database: "o2d39zh27oq2jifw",
   port: 3306,
   dialect: "mysql",
   protocol: "mysql",
