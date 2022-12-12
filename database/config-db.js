94e746d2217dbee8c75740648d398ec00d0aaeaf
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-12-12 00:19:50 -0300

    database config

--- database/config-db.js
+++ database/config-db.js
@@ -1,8 +1,10 @@
 module.exports = {
-  host: "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "fcf6apmfhdutisij",
-  password: "sz0ygtwq60zzl4cz",
-  database: "o2d39zh27oq2jifw",
+  // mysql://b2079f52a2a0bf:18b20432@us-cdbr-east-06.cleardb.net/heroku_2073765da9587cd?reconnect=true
+
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b2079f52a2a0bf",
+  password: "18b20432",
+  database: "heroku_2073765da9587cd",
   port: 3306,
   dialect: "mysql",
   protocol: "mysql",
